37a5ab96b49f3070625a27cab3a4c6a0c88fed3b
Артём <chipalinchik@gmail.com>
2022-08-07 01:10:14 +0500

    new data

--- package/gendiff.py
+++ package/gendiff.py
@@ -1,18 +1,13 @@
-# file <gendif.py>
+# file <gendiff.py>
 import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
-
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', metavar='first_file')
+    parser.add_argument('second_file', metavar='second_file')
+    parser.add_argument('-f', '--format', help='set format of output',)
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
-
 
 
 if __name__ == '__main__':
