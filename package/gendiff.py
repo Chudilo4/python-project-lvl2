3c54ae220bc5da366ba6c6c29e150088ce565a22
Chudilo4 <chipalinchik@gmail.com>
2022-08-08 14:09:07 +0300

    add str() in dict

--- package/gendiff.py
+++ package/gendiff.py
@@ -13,10 +13,10 @@ def generate_diff(file1, file2):
     for i in l:
         if i in f.keys():
             if f[i] in f2.values():
-                txt += f'\t\n   {i}: {f[i]}'
+                txt += f'\t\n   {i}: {str(f[i]).lower()}'
                 continue
-            txt += f'\t\n - {i}: {f[i]}'
+            txt += f'\t\n - {i}: {str(f[i]).lower()}'
         if i in f2.keys():
-            txt += f'\t\n + {i}: {f2[i]}'
+            txt += f'\t\n + {i}: {str(f2[i]).lower()}'
     txt += "\n}"
     return txt
