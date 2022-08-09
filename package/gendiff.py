5ce3c333b3932952cbd498a263a3317ba93458d4
Chudilo4 <chipalinchik@gmail.com>
2022-08-09 11:39:05 +0300

    add \n end fuction

--- package/gendiff.py
+++ package/gendiff.py
@@ -13,10 +13,10 @@ def generate_diff(file1, file2):
     for i in list_json_set:
         if i in f.keys():
             if f[i] in f2.values():
-                txt += f'\t\n   {i}: {str(f[i]).lower()}'
+                txt += f'\n    {i}: {str(f[i]).lower()}'
                 continue
-            txt += f'\t\n - {i}: {str(f[i]).lower()}'
+            txt += f'\n  - {i}: {str(f[i]).lower()}'
         if i in f2.keys():
-            txt += f'\t\n + {i}: {str(f2[i]).lower()}'
-    txt += "\n}"
+            txt += f'\n  + {i}: {str(f2[i]).lower()}'
+    txt += "\n}\n"
     return txt
