8d9951c15ca9fac8479c5253fa10563f0f3d0cae
Chudilo4 <chipalinchik@gmail.com>
2022-08-09 11:39:56 +0300

    add test gendif_main

--- tests/test_gendiff_main.py
+++ tests/test_gendiff_main.py
@@ -2,12 +2,5 @@ from package.gendiff import generate_diff
 
 
 def test_generate_diff():
-    assert generate_diff('tests/file1.json', 'tests/file2.json') == str({
-        - follow: false
-          host: hexlet.io
-        - proxy: 123.234.53.22
-        - timeout: 50
-        + timeout: 20
-        + verbose: true
-        })
-                    
+    f = open('tests/text.txt', 'r')
+    assert generate_diff('tests/file1.json', 'tests/file2.json') == f.read()
