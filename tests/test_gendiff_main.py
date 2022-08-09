d91ed9b94c56aea302251adac0997647f2cccf9f
Chudilo4 <chipalinchik@gmail.com>
2022-08-09 15:45:48 +0300

    fix gendiff

--- tests/test_gendiff_main.py
+++ tests/test_gendiff_main.py
@@ -3,4 +3,5 @@ from package.gendiff import generate_diff
 
 def test_generate_diff():
     f = open('tests/fixtures/text.txt', 'r')
-    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == f.read()
+    assert generate_diff('tests/fixtures/file1.json',
+                         'tests/fixtures/file2.json') == f.read()
