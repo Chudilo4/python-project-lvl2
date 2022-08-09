fcb474e277897c8e8884550e1ce7069cff3d6fa1
Chudilo4 <chipalinchik@gmail.com>
2022-08-09 15:43:01 +0300

    change path argument

--- tests/test_gendiff_main.py
+++ tests/test_gendiff_main.py
@@ -2,5 +2,5 @@ from package.gendiff import generate_diff
 
 
 def test_generate_diff():
-    f = open('tests/text.txt', 'r')
-    assert generate_diff('tests/file1.json', 'tests/file2.json') == f.read()
+    f = open('tests/fixtures/text.txt', 'r')
+    assert generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json') == f.read()
