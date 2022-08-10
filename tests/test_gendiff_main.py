3a5ce49fb4b1f5275eee14cfcbadbd22c5aa6e91
Chudilo4 <chipalinchik@gmail.com>
2022-08-10 18:15:50 +0300

    add test gendiff yaml

--- tests/test_gendiff_main.py
+++ tests/test_gendiff_main.py
@@ -1,7 +1,13 @@
-from package.gendiff import generate_diff
+from package.gendiff_json import generate_diff_json
+from package.gendiff_yaml import generate_diff_yaml
 
 
-def test_generate_diff():
-    f = open('tests/fixtures/text.txt', 'r')
-    assert generate_diff('tests/fixtures/file1.json',
-                         'tests/fixtures/file2.json') == f.read()
+def test():
+    f = open('tests/fixtures/test_json.txt', 'r')
+    f2 = open('tests/fixtures/test_yaml.txt', 'r')
+    test_json = generate_diff_json('tests/fixtures/file1.json',
+                                   'tests/fixtures/file2.json')
+    test_yaml = generate_diff_yaml('tests/fixtures/file3.yaml',
+                                   'tests/fixtures/file4.yaml')
+    assert test_json == f.read()
+    assert test_yaml == f2.read()
