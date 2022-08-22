d52e863f35c5ee91dbb51448039bca8fa8a8ff7c
Артём <chipalinchik@gmail.com>
2022-08-22 22:43:10 +0500

    рекурсивный поиск различий по дереву

--- package/include/render.py
+++ package/include/render.py
@@ -9,18 +9,18 @@ def render(value, depth=0):
     ident = step * depth
     delet_step = '  - '
     add_step = '  + '
-    txt = "{"
+    txt = "{\n"
     for i in value:
         if i[2] == 'del':
-            txt += '\n' + f'{ident}{delet_step}{i[0]}: {check_dict_value(i[1], step, depth+1)}'
+            txt += f'{ident}{delet_step}{i[0]}: {check_dict_value(i[1], step, depth+1)}' + '\n'
         elif i[2] == 'not change':
-            txt += '\n' + f'{ident}{step}{i[0]}: {check_dict_value(i[1], step, depth+1)}'
+            txt += f'{ident}{step}{i[0]}: {check_dict_value(i[1], step, depth+1)}' + '\n'
         elif i[2] == 'add':
-            txt += '\n' + f'{ident}{add_step}{i[0]}: {check_dict_value(i[3], step, depth+1)}'
+            txt += f'{ident}{add_step}{i[0]}: {check_dict_value(i[3], step, depth+1)}' + '\n'
         elif i[2] == 'changed':
-            txt += '\n' + f'{ident}{delet_step}{i[0]}: {check_dict_value(i[1], step, depth+1)}'
-            txt += '\n' + f'{ident}{add_step}{i[0]}: {check_dict_value(i[3], step, depth+1)}'
+            txt += f'{ident}{delet_step}{i[0]}: {check_dict_value(i[1], step, depth+1)}' + '\n'
+            txt += f'{ident}{add_step}{i[0]}: {check_dict_value(i[3], step, depth+1)}' + '\n'
         elif i[2] == 'have children':
-            txt += '\n' + f'{ident}{step}{i[0]}: {render(i[4], depth + 1)}'
-    txt += '\n' + f'{ident}}}'
+            txt += f'{ident}{step}{i[0]}: {render(i[4], depth + 1)}'
+    txt += f'{ident}}}\n'
     return txt
